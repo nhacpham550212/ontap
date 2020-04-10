5cbffaa459c9261e571a0d8ce379b4ddb95e0b7d
Administrator <nhacpham550212@gmail.com>
2020-04-10 14:32:00 +0700

    v1.0

---
+++ src/pkg59cntt3_interfacedemo/ICalculate.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg59cntt3_interfacedemo;
+
+/**
+ *
+ * @author Administrator
+ */
+public interface ICalculate {
+    public int cong(int s1, int s2);
+    public int tru(int s1, int s2);
+    public int nhan(int s1,int s2);
+    public float chia(int s1, int s2);
+}
